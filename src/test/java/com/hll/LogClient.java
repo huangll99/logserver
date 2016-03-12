7ceb785d13f05793223a32a0bbdfef6298ba4f16
hunagll <hunagll@qq.com>
2016-03-12 19:39:47 +0800

    多线程发日志

---
+++ src/test/java/com/hll/LogClient.java
@@ -0,0 +1,44 @@
+package com.hll;
+
+import org.apache.commons.codec.Charsets;
+import org.apache.http.HttpHost;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+/**
+ * Created by hll on 2016/3/12.
+ */
+public class LogClient {
+
+  private static Logger logger = LoggerFactory.getLogger(LogClient.class);
+
+  public static void main(String[] args)  {
+
+    for (int t = 0; t < 4; t++) {
+      new Thread(() -> {
+        for (int i = 0; i < 1000; i++) {
+          CloseableHttpClient client = HttpClients.createDefault();
+          HttpPost httpPost = new HttpPost("http://localhost:9999");
+          StringEntity stringEntity = new StringEntity("hello-" + i, Charsets.UTF_8);
+          httpPost.setEntity(stringEntity);
+          CloseableHttpResponse response = null;
+          try {
+            response = client.execute(httpPost);
+            logger.info(response.toString());
+            client.close();
+          } catch (IOException e) {
+            e.printStackTrace();
+          }
+
+        }
+      }).start();
+    }
+  }
+}
